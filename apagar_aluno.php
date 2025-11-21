dd7de44b921665511abf093e921c33cf3e2de78d
Arthur0292 <arthurvitordasilva01@gmail.com>
2025-11-21 12:09:49 -0300

    Refactor apagar_aluno.php for improved readability

--- apagar_aluno.php
+++ apagar_aluno.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-
+//Conexão com o BD
 $servername = "sql308.infinityfree.com";
 $username = "if0_40474741";
 $password = "8fJzevkLl5EJlQw";
@@ -39,4 +39,5 @@ if ($stmt->execute()) {
 
 $stmt->close();
 $conn->close();
+
 ?>
